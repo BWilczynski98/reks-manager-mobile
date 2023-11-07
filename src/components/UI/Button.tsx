a6465d45e1f4de7b4d92293db596a3d1e147b7fd
Bartłomiej Wilczyński <bartek.wilczynski@icloud.com>
2023-11-07 16:39:34 +0100

    New variant for button

--- src/components/UI/Button.tsx
+++ src/components/UI/Button.tsx
@@ -1,20 +1,30 @@
+import { cn } from "@/lib/cn";
 import React from "react";
-import { TouchableOpacity, View, Text, ActivityIndicator } from "react-native";
+import { ActivityIndicator, Text, TouchableOpacity, View } from "react-native";
 
 type ButtonProps = {
   children: React.ReactNode;
   onPress?: () => void;
   isLoading?: boolean;
+  variant?: "primary" | "outline";
 };
 
-export const Button = ({ children, onPress, isLoading }: ButtonProps) => {
+export const Button = ({ children, onPress, isLoading, variant = "primary" }: ButtonProps) => {
+  const outline = variant === "outline";
+
   return (
     <TouchableOpacity activeOpacity={0.8} onPress={onPress}>
-      <View className="bg-violet-700 rounded-lg h-10 justify-center items-center">
+      <View
+        className={cn("bg-violet-700 rounded-lg h-10 justify-center items-center", {
+          "bg-transparent": outline,
+          border: outline,
+          "border-gray-700": outline,
+        })}
+      >
         {isLoading ? (
           <ActivityIndicator size="small" />
         ) : (
-          <Text className="text-gray-50 text-base text-center">{children}</Text>
+          <Text className={cn("text-gray-50 text-base text-center")}>{children}</Text>
         )}
       </View>
     </TouchableOpacity>
