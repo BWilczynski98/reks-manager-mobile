10219845785390fe2d1744ef6eec1ac5be2bc0f4
Bartłomiej Wilczyński <bartek.wilczynski@icloud.com>
2023-11-25 22:03:29 +0100

    Designation of screen names

---
+++ src/navigation/screenNames.ts
@@ -0,0 +1,8 @@
+export enum ScreenNames {
+  SIGN_IN = "SignIn",
+  FORGOT_PASSWORD = "ForgotPassword",
+  DASHBOARD = "Dashboard",
+  ADD_ANIMAL_FORM = "AddAnimalForm",
+  SETTINGS = "Settings",
+  ANIMAL_PROFILE = "AnimalProfile",
+}
