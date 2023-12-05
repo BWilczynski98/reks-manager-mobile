234fdbb991b655732f0103e703e674476ac9ac79
Bartłomiej Wilczyński <bartek.wilczynski@icloud.com>
2023-12-05 09:47:38 +0100

    updat export

--- src/screens/index.ts
+++ src/screens/index.ts
@@ -2,4 +2,5 @@ export { AnimalProfile } from "./AnimalProfile";
 export { CreateProfile } from "./CreateProfile";
 export { Dashboard } from "./Dashboard";
 export { ForgotPassword } from "./ForgotPassword";
+export { HealthCard } from "./HealthCard";
 export { SignIn } from "./SignIn";
