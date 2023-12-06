4131e5797a9f2bf43efc75f6102b96ce0f265725
Bartłomiej Wilczyński <bartek.wilczynski@icloud.com>
2023-12-06 14:03:01 +0100

    Error for matching passwords

--- src/helpers/errors-dictionary.ts
+++ src/helpers/errors-dictionary.ts
@@ -3,4 +3,5 @@ export const errorsDictionary = {
   not_valid_email_address: "Adres email jest nie poprawny",
   not_allowed_signs: "Pole zawiera niedozwolone znaki",
   incorrect_date: "Podana data jest nie poprawna",
+  password_not_match: "Hasła muszą być takie same",
 };
