048eaed9510fbef823ba292893c2ad65c22e997d
Bartłomiej Wilczyński <bartek.wilczynski@icloud.com>
2023-12-10 15:01:44 +0100

    update eas envs

--- redux/emptyApi.ts
+++ redux/emptyApi.ts
@@ -1,11 +1,11 @@
-import { BASE_URL } from "@env";
+import { EXPO_PUBLIC_API_URL } from "@env";
 import { createApi, fetchBaseQuery } from "@reduxjs/toolkit/query/react";
 import { RootState } from "./store";
 import * as SecureStore from "expo-secure-store";
 
 export const emptySplitApi = createApi({
   baseQuery: fetchBaseQuery({
-    baseUrl: BASE_URL,
+    baseUrl: EXPO_PUBLIC_API_URL,
     prepareHeaders: async (headers, { getState, endpoint }) => {
       const token = await SecureStore.getItemAsync("token");
       if (token) {
